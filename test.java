092567b6627fc3bfdf2a56a60f38fac231f07494
Yashraj119 <yashrajsoni936@gmail.com>
2026-02-27 00:15:02 +0530

    Ychange

--- test.java
+++ test.java
@@ -2,7 +2,7 @@ class test{
     public static void main(String args[]){
         System.out.print("Hello");
         System.out.println("tari maasi ne pranam");
-        int a=5;
+        int a=10;
         System.out.print(a);
     }
 }
